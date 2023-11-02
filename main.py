ade44d02d1c15a0f29be29732bb226ee34045d87
Xander <XanderG2@users.noreply.github.com>
2023-11-02 17:00:50 +0000

    fix grammar

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 go = "YES"
 while go != "NO":
     print("-----------RESTART--------------RESTART----------------RESTART----------")
-
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ "
-    stringtoencrypt = input ("please enter a message to encrypt:")
+    stringtoencrypt = input ("Please enter a message to encrypt: ")
     stringtoencrypt = stringtoencrypt.upper()
-    shiftamount = int(input("Please enter a whole number from 1-25 to be your key."))
+    shiftamount = int(input("Please enter a whole number from 1-25 to be your key: "))
     encrypyrdstring = ""
     for currentcharacter in stringtoencrypt:
         position = alphabet.find(currentcharacter)
@@ -14,11 +13,11 @@ while go != "NO":
             encrypyrdstring = encrypyrdstring + alphabet[newposition]
         else:
             encrypyrdstring = encrypyrdstring + alphabet[newposition]
-        print(" your encrypted new message is", encrypyrdstring)
+        print("Your encrypted new message is", encrypyrdstring)
     print ("---------------------------------------------------------------------")
     print ()
-    print(" Your message is", stringtoencrypt)
-    print(" your encrypted message is", encrypyrdstring)  
+    print("Your message is", stringtoencrypt)
+    print("Your encrypted message is", encrypyrdstring)  
     print ()
     print ("--------------------------------------------------------------------")
-    go = input ("Press enter to to start again, or type No to leave").upper()
+    go = input ("Press enter to to start again, or type no to leave").upper()
